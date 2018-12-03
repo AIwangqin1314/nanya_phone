352b466235e2a22a46df07e79adc31e6180955fe
AIwangqin1314 <39695065+AIwangqin1314@users.noreply.github.com>
2018-12-03 22:13:48 +0800

    真加验证 手机

--- src/components/Postmsg.vue
+++ src/components/Postmsg.vue
@@ -14,12 +14,20 @@
         <van-field
                 v-model="phone"
                 label="手机号"
+                type="tel"
                 placeholder="请输入手机号"
 
         />
+        <van-field
+                v-model="cell_value"
+                label="礼物"
+                type="tel"
+                placeholder="请点击选择"
+                @click="change_picker_show"
+        />
     </van-cell-group>
         <br>
-    <van-cell  v-model="cell_value" title="礼物" is-link arrow-direction="down" @click="change_picker_show" />
+    <!--<van-cell  v-model="cell_value" title="礼物" is-link arrow-direction="down" @click="change_picker_show" />-->
             <van-button type="primary" @click="requestData">提交</van-button>
             <van-popup v-model="show" position="bottom" :overlay="false">
                 <van-picker
@@ -63,21 +71,50 @@
             postmsg_button(){
                 this.$router.push({ path:'/Postmsgruslui'})
             },
-            requestData(){
-        this.$ajax.get('http://localhost:8080/Nayajavaee/Houtai',{
-            params:{
-                name:this.name_uer,
-                phone:this.phone,
-                gift:this.cell_value
+            requestData(){if (this.name_uer ==="") {
+                this.$toast("姓名不能空");
+            }else if (this.phone===''){
+                this.$toast("电话不能空");
+            }else if (this.cell_value===""){
+                this.$toast("礼物未选择");
+            }else {
+                if (!/^1[3456789]\d{9}$/.test(this.phone)) {
+                    this.$toast("手机号格式不对");
+                }else if (/[0-9]/.test(this.name_uer)){
+                    this.$toast("姓名不能包含数字");
+                } else {
+                    this.$ajax.get('http://192.204.65.155:8080/Nayajavaee/Houtai',{
+                        params:{
+                            name:this.name_uer,
+                            phone:this.phone,
+                            gift:this.cell_value
+                        }
+                    })
+                        .then(resp => {
+                            console.log(resp.data);
+                            // if(resp.data.count_num)this.$router.push({ path:'/Postmsgruslui'});
+                            // else
+                            alert(resp.data.count_num);
+                            this.$toast(`目前一共有：${resp.data.count_num}人跟您有共同愿望`);
+                        }).catch(err => {             //
+                        console.log('请求失败：'+err.status+','+err.statusText);
+                    });
+                }
             }
-        })
-            .then(resp => {
-                console.log(resp.data);
-                if(resp.data.id)this.$router.push({ path:'/Postmsgruslui'});
-                else  this.$toast(`当前值：${resp.data.id}`);
-            }).catch(err => {             //
-            console.log('请求失败：'+err.status+','+err.statusText);
-        });
+        // this.$ajax.get('http://localhost:8080/Nayajavaee/Houtai',{
+        //     params:{
+        //         name:this.name_uer,
+        //         phone:this.phone,
+        //         gift:this.cell_value
+        //     }
+        // })
+        //     .then(resp => {
+        //         console.log(resp.data);
+        //         if(resp.data.id)this.$router.push({ path:'/Postmsgruslui'});
+        //         else  this.$toast(`当前值：${resp.data.id}`);
+        //     }).catch(err => {             //
+        //     console.log('请求失败：'+err.status+','+err.statusText);
+        // });
     },
         }
 
