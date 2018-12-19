706e50c798e2a223f228208ff0bf0a859dfdc96c
AIwangqin1314 <39695065+AIwangqin1314@users.noreply.github.com>
2018-12-19 10:06:48 +0800

    以东溟

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    devServer: {
+        // 设置代理
+        proxy: {
+            '/app': {
+                // 目标 API 地址
+                target: 'http://nxha.ynnsd.com/api/v1/Nymc/index.aspx',
+                // 如果要代理 websockets
+                ws: true,
+                // 将主机标头的原点更改为目标URL
+                changeOrigin: true,
+                //这个必须写啊。。。。（我也不知道为啥，不写就错了）
+                pathRewrite:{
+                    '^/app':""
+                }
+            }
+        }
+    }
+}
