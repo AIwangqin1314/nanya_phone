a9683d0d75086656a00e9cbaccbf6aee397ad35c
AIwangqin1314 <39695065+AIwangqin1314@users.noreply.github.com>
2018-12-22 21:35:45 +0800

    背景

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="app">
-    <div>
+
       <van-row type="flex" class="top_button">
         <!--<router-link to="/Login">返回</router-link>-->
         <!--<router-link to="/get_id">控制界面</router-link>-->
       </van-row>
-    </div>
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <!--<router-link to="/HelloWorld">1</router-link>-->
-    <!--<router-link to="/Postmsg">返回</router-link>-->
+
     <router-view></router-view>
 
   </div>
@@ -36,6 +32,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 30px;
+  height: 100vh;
+  background: url("./assets/bk.jpg");
+  background-size: 100% 100%;
 }
   .top_button{
       position: fixed;
